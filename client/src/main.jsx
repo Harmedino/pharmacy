eca7bcf07e5482c0b7755d8fa5a18909dbe9beb1
Harmedino <adamsahmed191@gmail.com>
2026-01-11 14:34:26 +0100

    fix

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,20 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { AppContextProvider } from './contex/AppContex.jsx'
+import { ClerkProvider } from '@clerk/clerk-react'
+
+const PUBLISHABLE_KEY ='pk_test_ZnVuLXNvbGUtNzEuY2xlcmsuYWNjb3VudHMuZGV2JA'
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Add your Clerk Publishable Key to the .env file')
+}
 
 createRoot(document.getElementById('root')).render(
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
   <BrowserRouter>
+  
   <AppContextProvider>
     <App />
     </AppContextProvider>
-  </BrowserRouter>,
+  </BrowserRouter>
+  </ClerkProvider>
 )
